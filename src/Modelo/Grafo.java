c4cbd446c443cfaea4bcd98559ce9e09107dd65b
JuanPValdivia <juan_valdivia_1993@outlook.com>
2018-08-29 20:10:18 -0300

    iniciaizacion de grafo
    generar grafo, azar y tamaño

--- src/Modelo/Grafo.java
+++ src/Modelo/Grafo.java
@@ -3,12 +3,47 @@
  */
 package Modelo;
 
+import java.util.Random;
+
 /**
  * @author juan_
  *
  */
 public class Grafo {
 
+	private int n;
+	private int[][] GMatriz;
 	
+	public Grafo(int n) {
+		this.n = n;
+		GMatriz = new int[this.n][this.n];
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<n;j++) {
+				GMatriz[i][j]=0;
+			}
+		}
+	}
+	
+	public void add_peso(int i,int j,int d) {
+		GMatriz[i][j]=d;
+	}
+	
+	public void del_peso(int i, int j) {
+		GMatriz[i][j]=-1;
+	}
+	
+	public void matriz_azar() {
+		for(int i=0;i<this.n;i++) {
+			for(int j=0;j<this.n;j++) {
+				GMatriz[i][j] = new Random().nextInt(10);
+			}
+		}
+		for(int i=0;i<this.n;i++) {
+			for(int j=0;j<this.n;j++) {
+				System.out.println(GMatriz[i][j]);
+			}
+		}
+		
+	}
 
 }
