f57e6158629a2aec64a3cc6d4cf2a7940d840f48
JuanPValdivia <juan_valdivia_1993@outlook.com>
2018-08-29 20:50:50 -0300

    Dijkstra

---
+++ src/Modelo/Dijkstra.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package Modelo;
+
+/**
+ * @author juan_
+ *
+ */
+public class Dijkstra {
+
+	/**
+	 * 
+	 */
+	public Dijkstra() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
