a696be6796b32198bd1fdd347173e9d94ab2e80a
JuanPValdivia <juan_valdivia_1993@outlook.com>
2018-08-30 03:48:53 -0300

    Metodo de Dijkstra
    de implemento el algoritmo en base al grafo por matriz, se realizaron pruebas de su búsqueda y entrega de datos, en este caso los pasos por consola y el peso menor final, en una lista con los pesos que fue calculando paso a paso

--- src/Modelo/Dijkstra.java
+++ src/Modelo/Dijkstra.java
@@ -3,17 +3,62 @@
  */
 package Modelo;
 
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+
 /**
  * @author juan_
  *
  */
 public class Dijkstra {
+	
+	ArrayList<Integer> lista;
 
 	/**
 	 * 
 	 */
 	public Dijkstra() {
-		// TODO Auto-generated constructor stub
+		this.lista = new ArrayList<>();
+	}
+	
+	public ArrayList<Integer> BusquedaMejorCamino(Grafo m,int fin){
+		ArrayList<Integer> padres = new ArrayList<Integer>();
+		
+		lista.add(0);
+		padres.add(0);
+
+		for(int i = 1; i < fin+1 ; i++) {
+			lista.add(1000000000);
+			padres.add(-1);
+		}
+		PriorityQueue<Integer> pila = new PriorityQueue<>();
+		pila.add(lista.get(0));
+		while(!pila.isEmpty()) {
+			System.out.println(pila);
+			int j=pila.poll();
+			for(int h=0;h<fin+1;h++) {
+				if(m.getGMatriz()[j][h] != 0) {
+					if(lista.get(h) > lista.get(j) + m.getGMatriz()[j][h]) {
+						lista.set(h, lista.get(j) + m.getGMatriz()[j][h]);
+						System.out.println(j+"->"+h+" = "+m.getGMatriz()[j][h]);
+						padres.set(h, j);
+						pila.add(h);
+					}
+				}
+			}
+		}
+		return lista;
+	}
+
+	/**
+	 * @return the lista
+	 */
+	public ArrayList<Integer> getLista() {
+		return lista;
+	}
+
+	public String toString() {
+		return ""+lista;
 	}
 
 }
